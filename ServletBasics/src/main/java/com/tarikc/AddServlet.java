e651606260dccf25b3dd1c694705d27c7edd4b06
unknown <tarikcuric97@gmail.com>
2024-01-10 14:47:08 +0100

    'SessionManagment'

--- ServletBasics/src/main/java/com/tarikc/AddServlet.java
+++ ServletBasics/src/main/java/com/tarikc/AddServlet.java
@@ -7,6 +7,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 public class AddServlet extends HttpServlet {
 	public void service(HttpServletRequest req,HttpServletResponse res) throws IOException, ServletException {
@@ -14,11 +15,17 @@ public class AddServlet extends HttpServlet {
 		int b2= Integer.parseInt(req.getParameter("num2"));
 		int rez=b1+b2;
 		
-		req.setAttribute("rez", rez);
-		
-		res.getWriter().println("Result  is:"+rez);
-		RequestDispatcher rd = req.getRequestDispatcher("square");
-		rd.forward(req, res);
+		//Redirect	
+		HttpSession session = req.getSession();//Sessions
+		session.setAttribute("rez", rez);
+		res.sendRedirect("square");
+//		res.sendRedirect("square?b1="+b1);//Url rewritting
+//		
+//		Request Dispatcher
+//		req.setAttribute("rez", rez);
+//		res.getWriter().println("Result  is:"+rez);
+//		RequestDispatcher rd = req.getRequestDispatcher("square");
+//		rd.forward(req, res);
 		
 	}
 }
--- ServletBasics/src/main/java/com/tarikc/SquareServlet.java
+++ ServletBasics/src/main/java/com/tarikc/SquareServlet.java
@@ -5,12 +5,26 @@ import java.io.IOException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 public class SquareServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException  {
 		 
-		int rez = (int)req.getAttribute("rez");
-		rez = rez*rez;
+		//Redirect
+		
+		//Session
+		HttpSession session = req.getSession();
+		int rez = (int)session.getAttribute("rez");
+		rez=rez*rez;
+		//Url rewritting
+//		int b1 = Integer.parseInt(req.getParameter("b1"));
+//		b1=b1*b1;
+//		res.getWriter().println("Square:"+b1); 
+		
+		
+		//RequestDispatcher
+//		int rez = (int)req.getAttribute("rez");
+//		rez = rez*rez;
 		res.getWriter().println("Square is:"+rez);
 	}
 }
