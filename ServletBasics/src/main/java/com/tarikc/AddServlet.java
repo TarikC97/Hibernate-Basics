e0fc5e473649e7e12e93d2b9383a1aef6e1f3deb
unknown <tarikcuric97@gmail.com>
2024-01-10 11:40:13 +0100

    'SessionManagment'

--- ServletBasics/src/main/java/com/tarikc/AddServlet.java
+++ ServletBasics/src/main/java/com/tarikc/AddServlet.java
@@ -2,17 +2,23 @@ package com.tarikc;
 
 import java.io.IOException;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet {
-	public void service(HttpServletRequest req,HttpServletResponse res) throws IOException {
+	public void service(HttpServletRequest req,HttpServletResponse res) throws IOException, ServletException {
 		int b1 = Integer.parseInt(req.getParameter("num1"));
 		int b2= Integer.parseInt(req.getParameter("num2"));
 		int rez=b1+b2;
+		
+		req.setAttribute("rez", rez);
+		
 		res.getWriter().println("Result  is:"+rez);
 		RequestDispatcher rd = req.getRequestDispatcher("square");
+		rd.forward(req, res);
 		
 	}
 }
--- ServletBasics/src/main/java/com/tarikc/SquareServlet.java
+++ ServletBasics/src/main/java/com/tarikc/SquareServlet.java
@@ -1,11 +1,16 @@
 package com.tarikc;
 
+import java.io.IOException;
+
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class SquareServlet extends HttpServlet {
-	public void doGet(HttpServletRequest req,HttpServletResponse res) {
-		 res.getWriter().println("Test");
+	public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException  {
+		 
+		int rez = (int)req.getAttribute("rez");
+		rez = rez*rez;
+		res.getWriter().println("Square is:"+rez);
 	}
 }
