d6a65a16d7e530420996f53add5790ff44970266
unknown <tarikcuric97@gmail.com>
2024-03-19 14:32:26 +0100

    'ServeltFilters'

--- ServletBasics/src/main/java/com/tarikc/JSTL.java
+++ ServletBasics/src/main/java/com/tarikc/JSTL.java
@@ -7,15 +7,17 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 
 @WebServlet("/jstl")
 public class JSTL extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//JSTL
-		String name = "TarikC";
-		
-		request.setAttribute("ime", name);
+		//JSTL)
+		List<String> studenti = Arrays.asList("TarikC","AsijaC","MensurBrat");
+
+		request.setAttribute("imena", studenti);
 		RequestDispatcher rd = request.getRequestDispatcher("display.jsp");
 		rd.forward(request, response);
 	}
--- ServletBasics/src/main/webapp/display.jsp
+++ ServletBasics/src/main/webapp/display.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%> 
+    pageEncoding="ISO-8859-1"%>
+    
   
 <!DOCTYPE html>
 <html>
@@ -8,7 +9,7 @@
 <title>Insert title here</title>
 </head>
 <body>	
-	${ime}<br>
+	${imena}<br>
 	
 	
 	
