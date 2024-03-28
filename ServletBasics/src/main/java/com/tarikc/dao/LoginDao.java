1d139346d040c9fef5e143489d3b055ee46a9c92
unknown <tarikcuric97@gmail.com>
2024-03-28 01:53:01 +0100

    'JDBC'

--- ServletBasics/src/main/java/com/tarikc/dao/LoginDao.java
+++ ServletBasics/src/main/java/com/tarikc/dao/LoginDao.java
@@ -8,16 +8,16 @@ import java.sql.ResultSet;
 public class LoginDao {
 	
 	String query = "select * from login where username=? and password=?";
-	String url = "jdbc:mysql://localhost:3308/servlet?useSSL=false&useUnicode=yes&characterEncoding=UTF-8&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+	String url = "jdbc:mysql://localhost:3308/servlet?useSSL=false&allowPublicKeyRetrieval=true";
 	String username = "root";
 	String password = "drumpanorama97";
-	public boolean checkUser(String username, String pass ) {
+	public boolean checkUser(String name, String pass ) {
 		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection con = DriverManager.getConnection(url,username,password);
 			PreparedStatement st = con.prepareStatement(query);
-			st.setString(1, username);
+			st.setString(1, name);
 			st.setString(2, pass);
 			ResultSet rs = st.executeQuery();
 			if(rs.next()) {
