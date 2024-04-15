c5cb60f7f8caefd1866dae7a159e72581e75bd72
Lenovo <tarikcuric97@gmail.com>
2024-04-15 13:27:09 +0200

    HibernateIntro

---
+++ DemoHibernate/src/main/java/com/tarikc/DemoHibernate/App.java
@@ -0,0 +1,16 @@
+package com.tarikc.DemoHibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Student std = new Student();
+        std.setId(1);
+        std.setName("Tarik");
+        std.setSurname("Curic");
+    }
+}
